3736b4e6d41888309fe9fd1cb0897b60c8a6fc6c
Adrien Souquere <adriensouquere@gmail.com>
2020-05-29 16:09:52 +0200

    progression login + spring security + création user

---
+++ soclewarthymeleaf/src/main/java/com/soclewarthymeleaf/soclewarthymeleaf/conf/WebSecurityConfig.java
@@ -0,0 +1,46 @@
+package com.soclewarthymeleaf.soclewarthymeleaf.conf;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+	    http
+	    	.authorizeRequests()
+	        .antMatchers("/login1/**").permitAll()
+	        .antMatchers("/accueil/**").hasAnyRole("ADMIN","USER")
+	            .and()
+	        .formLogin()
+	        .loginPage("/login.html")
+	        .defaultSuccessUrl("/accueil", true)
+	            .and()
+	        .logout()
+	        .logoutSuccessUrl("/login")
+	        .logoutUrl("/logout")
+	        	.and()
+	        .httpBasic();
+    }
+		
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		auth.inMemoryAuthentication()
+	    .withUser("prothelem")
+	        .password(encoder.encode("7nc5AFz8M"))
+	        .roles("ADMIN")
+	        .and()
+	    .withUser("employethelem")
+	        .password(encoder.encode("Br8T72sXf"))
+	        .roles("USER");
+	}
+	
+}	
